c6c69fac1a1580b59d2d2a6d8fd86dd2ec84a102
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-06-22 15:06:37 +0000

    add unionfind

---
+++ src/unionfind.d
@@ -0,0 +1,74 @@
+import std.algorithm;
+
+/// Union-Find
+struct UnionFind(T) {
+
+    /// Constructor
+    this(T n) {
+        len = n;
+        par.length = len;
+        cnt.length = len;
+        foreach (i; 0 .. len) {
+            par[i] = i;
+        }
+        cnt[] = 1;
+    }
+
+    /// Returns the root of x.
+    T root(T x)
+    in (0 <= x && x < len) {
+        if (par[x] == x) {
+            return x;
+        }
+        else {
+            return par[x] = root(par[x]);
+        }
+    }
+
+    /// Returns whether x and y have the same root.
+    bool isSame(T x, T y)
+    in (0 <= x && x < len && 0 <= y && y < len) {
+        return root(x) == root(y);
+    }
+
+    /// Unites x tree and y tree.
+    void unite(T x, T y)
+    in (0 <= x && x < len && 0 <= y && y < len) {
+        x = root(x), y = root(y);
+        if (x == y) {
+            return;
+        }
+
+        if (cnt[x] > cnt[y]) {
+            swap(x, y);
+        }
+
+        cnt[y] += cnt[x];
+        par[x] = y;
+    }
+
+    /// Returns the size of the x tree.
+    T size(T x)
+    in (0 <= x && x < len) {
+        return cnt[root(x)];
+    }
+
+private:
+    T len;
+    T[] par;
+    T[] cnt;
+}
+
+// https://atcoder.jp/contests/atc001/tasks/unionfind_a
+unittest {
+    auto uf = new UnionFind!int(8);
+    uf.unite(1, 2);
+    uf.unite(3, 2);
+    assert(uf.isSame(1, 3));
+    assert(!uf.isSame(1, 4));
+    uf.unite(2, 4);
+    assert(uf.isSame(4, 2));
+    uf.unite(4, 2);
+    uf.unite(0, 0);
+    assert(uf.isSame(0, 0));
+}
